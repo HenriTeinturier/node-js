91285f5b0ed1e6e0deb92f0fada3e425e977fa45
Teinturier <henri.teinturier@gmail.com>
2024-11-01 00:01:57 +0100

    test modules

---
+++ iife.js
@@ -0,0 +1,8 @@
+(function () {
+  const superHero = "Batman";
+  console.log(superHero);
+})();
+(function (message) {
+  const superHero = "Superman";
+  console.log(message, superHero);
+})("Hello");
