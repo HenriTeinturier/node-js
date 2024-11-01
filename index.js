0566598e9d1b3b462237be654ce08e93e9ef8c33
Teinturier <henri.teinturier@gmail.com>
2024-11-01 14:49:25 +0100

    syncCallback

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ function higherOrderFunction(callback) {
 }
 
 higherOrderFunction(greet);
+
+let numbers = [1, 2, 3, 4, 5];
+
+numbers.sort((a, b) => a - b);
+numbers.filter((number) => number % 2 === 0);
+numbers.map((number) => number * 2);
