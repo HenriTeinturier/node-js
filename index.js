03112e8b284c53559b0e606ceb2d6569d29452e4
Teinturier <henri.teinturier@gmail.com>
2024-11-01 14:03:44 +0100

    path module

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-// const data = require("./data.json");
-const data = require("./data");
+const path = require("path");
 
-console.log(data);
+console.log(path.resolve("folder1", "folder2", "index.html"));
+// => C:\Users\user\Desktop\nodejs\folder1\folder2\index.html
+
+console.log(path.resolve("/folder1", "folder2", "index.html"));
+// => folder1\folder2\index.html
+
+console.log(path.resolve("/folder1", "//folder2", "index.html"));
+// => folder2\index.html
+
+console.log(path.resolve("/folder1", "//folder2", "../index.html"));
+// => index.html
+
+console.log(path.resolve(__dirname, "data.json"));
+// => C:\Users\user\Desktop\nodejs\data.json
