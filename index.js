1f21fe60bb5311a8b82fe084f25086ea6301ae58
Teinturier <henri.teinturier@gmail.com>
2024-11-01 14:48:03 +0100

    callback

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-const path = require("path");
+function greet(name) {
+  console.log(`Hello ${name}`);
+}
 
-console.log(path.resolve("folder1", "folder2", "index.html"));
-// => C:\Users\user\Desktop\nodejs\folder1\folder2\index.html
+function higherOrderFunction(callback) {
+  callback("Henri");
+}
 
-console.log(path.resolve("/folder1", "folder2", "index.html"));
-// => folder1\folder2\index.html
-
-console.log(path.resolve("/folder1", "//folder2", "index.html"));
-// => folder2\index.html
-
-console.log(path.resolve("/folder1", "//folder2", "../index.html"));
-// => index.html
-
-console.log(path.resolve(__dirname, "data.json"));
-// => C:\Users\user\Desktop\nodejs\data.json
+higherOrderFunction(greet);
